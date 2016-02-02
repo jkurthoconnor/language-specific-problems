707ddd39cbe92ef021968d1abf61170fc7cb1873
jkurthoconnor <jkurthoconnor@gmail.com>
2016-02-02 12:28:28 -0700

    new files and renaming

--- ref_methods.rb
+++ ref_methods.rb
@@ -1,3 +1,17 @@
+a = [1, 2, 3, 4]
+
+def mutate(array)
+  array.pop
+end
+def not_mutated(array)
+  array.last
+end
+p "Array before calling mutate method: #{a}"
+mutate(a)
+p "Array after calling mutate method: #{a}"
+p "Array after calling the not_mutated method #{a}"
+
+
 def say(text = "This string is the default parameter, used when no argument is passed with method call.")
   puts "#{text}!!!"
 end
