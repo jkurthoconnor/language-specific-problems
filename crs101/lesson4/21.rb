6f7119e83bf351a75e907f5a9ac328e074d3174b
jkurthoconnor <jkurthoconnor@gmail.com>
2016-03-14 06:37:24 -0600

    add constants for goal & hold threshold; revise points forumula

--- crs101/lesson4/21.rb
+++ crs101/lesson4/21.rb
@@ -4,27 +4,34 @@
 # - too much repetition of 'n wins' text; find way to consolidate
 # - hide/show additional dealer cards?
 # - large conditional chains smell; fix if not remedied by adding the play again loop and breaks
-# - create deck without relying on hash of complete options
+# - *** create deck without relying on hash of complete options
 
 # functional additions
 # - XX play again loop
 # - xx entry validation
 # - xx display current hands at top of screen (use 'clear'?)
 # - xx add prompt
-# - add play to five loop
+# -  xx add play to five loop
+#   - xx add display results after final hand
+# - xx add constants for goal (now GOAL) and dealer hold threshold (now 17)
+#   - xx refactor formulae to accomodate outcomes with new goal
 
 
 require 'pry'
 
 DECK = { '2 of Hearts' => 2, '3 of Hearts' => 3, '4 of Hearts' => 4, '5 of Hearts' => 5, '6 of Hearts' => 6, '7 of Hearts' => 7, '8 of Hearts' => 8, '9 of Hearts' => 9, '10 of Hearts' => 10, 'Jack of Hearts' => 10, 'Queen of Hearts' => 10, 'King of Hearts' => 10, 'Ace of Hearts' => 11, '2 of Diamonds' => 2, '3 of Diamonds' => 3, '4 of Diamonds' => 4, '5 of Diamonds' => 5, '6 of Diamonds' => 6, '7 of Diamonds' => 7, '8 of Diamonds' => 8, '9 of Diamonds' => 9, '10 of Diamonds' => 10, 'Jack of Diamonds' => 10, 'Queen of Diamonds' => 10, 'King of Diamonds' => 10, 'Ace of Diamonds' => 11, '2 of Spades' => 2, '3 of Spades' => 3, '4 of Spades' => 4, '5 of Spades' => 5, '6 of Spades' => 6, '7 of Spades' => 7, '8 of Spades' => 8, '9 of Spades' => 9, '10 of Spades' => 10, 'Jack of Spades' => 10, 'Queen of Spades' => 10, 'King of Spades' => 10, 'Ace of Spades' => 11, '2 of Clubs' => 2, '3 of Clubs' => 3, '4 of Clubs' => 4, '5 of Clubs' => 5, '6 of Clubs' => 6, '7 of Clubs' => 7, '8 of Clubs' => 8, '9 of Clubs' => 9, '10 of Clubs' => 10, 'Jack of Clubs' => 10, 'Queen of Clubs' => 10, 'King of Clubs' => 10, 'Ace of Clubs' => 11 }
 
+GOAL = 21
+DEALER_HOLD_POINT = 17
+
 def prompt(message)
   puts "=> #{message}"
 end
 
 def introduce_game
-  prompt "Welcome to 21.  The first to win five hands wins the game."
-  sleep 2.0
+  system 'clear' or system 'cls'
+  prompt "Welcome to #{GOAL}.  The first to win five hands wins the game."
+  sleep 1.5
 end
 
 def shuffle_new_deck(source)
@@ -43,8 +50,8 @@ def update_hands_values(hands, totals)
   totals.each_key do |k|
     totals[k] = hands[k].flatten.select { |n| n == n.to_i }
                         .inject { |sum, n| sum + n }
-    if totals[k] > 21 && hands[k].flatten.include?(11)
-      totals[k] = (totals[k] - 10 * hands[k].flatten.count(11)) + 10
+    hands[k].flatten.count(11).times do
+      totals[k] -= 10 if totals[k] > GOAL
     end
   end
 end
@@ -54,14 +61,14 @@ def hit_me(hands, turn, deck)
 end
 
 def busted?(totals, turn)
-  if totals[turn] > 21
+  if totals[turn] > GOAL
     true
   end
 end
 
 def display_current_status(hands, score)
   system 'clear' or system 'cls'
-  prompt "Game Score: Dealer: #{score[:dealer]} Player: #{score[:player]}"
+  prompt "Game Score: Dealer: #{score[:dealer]} // Player: #{score[:player]}"
   puts
   prompt "Current Hand."
   prompt "Dealer's Cards: showing #{hands[:dealer][0][0]} and #{hands[:dealer]
@@ -72,10 +79,10 @@ def display_current_status(hands, score)
 end
 
 def determine_hand_outcome(totals)
-  comparison = 21 - totals[:player] <=> 21 - totals[:dealer]
-  if totals[:player] > 21
+  comparison = GOAL - totals[:player] <=> GOAL - totals[:dealer]
+  if totals[:player] > GOAL
     'player busts'
-  elsif totals[:dealer] > 21
+  elsif totals[:dealer] > GOAL
     'dealer busts'
   elsif comparison == -1
     'player'
@@ -98,9 +105,9 @@ def display_hand_outcome(hands, outcome, totals)
   case outcome
   when 'player' then prompt "Congratulations, you won!!"
   when 'tie' then prompt 'Wow!  That hand was a tie.'
-  when 'dealer' then prompt "I'm sorry.  The computer won. Better luck next time."
-  when 'player busts' then prompt "You went over 21.  The computer won."
-  when 'dealer busts' then prompt "The computer went over 21. Victory is yours!!"
+  when 'dealer' then prompt "I'm sorry.  The dealer won. Better luck next time."
+  when 'player busts' then prompt "You went over #{GOAL}.  The dealer won."
+  when 'dealer busts' then prompt "The dealer went over #{GOAL}. Victory is yours!!"
   end
   puts
   prompt "Hand Results"
@@ -110,25 +117,33 @@ def display_hand_outcome(hands, outcome, totals)
   prompt "Player's Hand: #{hands[:player].flatten.select { |c| c != c.to_i }
       .join(', ')}"
   prompt "Player's Points: #{totals[:player]}\n\n"
-  sleep 2.0
+  sleep 5.0
+end
+
+def display_game_outcome(score)
+  system 'clear' or system 'cls'
+  prompt "Final Score: Dealer: #{score[:dealer]} // Player: #{score[:player]}"
+  if score[:dealer] == 5
+    "I'm sorry.  The dealer won this game."
+  else
+    "Congratulations!  You beat the dealer!!"
+  end
 end
 
 loop do
   introduce_game
+  response = nil
 
-  loop do # play to 5
+  loop do
     scores = { dealer: 0, player: 0 }
-
-    loop do # individual hands
+    loop do
       deck_in_play = shuffle_new_deck(DECK)
       current_hands = { dealer: [], player: [] }
       current_hands_totals = { dealer: 0, player: 0 }
-      current_hand_outcome = nil
 
       deal_hands(deck_in_play, current_hands)
       display_current_status(current_hands, scores)
 
-      response = nil
       loop do
         prompt "Would you like to hit or stay?"
 
@@ -144,7 +159,7 @@ loop do
         prompt "Your new card is: #{current_hands[:player][-1][0]}"
         sleep 1.0
         display_current_status(current_hands, scores)
-        break if busted?(current_hands_totals, :player) || current_hands_totals[:player] == 21
+        break if busted?(current_hands_totals, :player) || current_hands_totals[:player] == GOAL
       end
 
       if response == 'stay'
@@ -153,8 +168,8 @@ loop do
       end
 
       loop do
-        break if busted?(current_hands_totals, :player) || (current_hands_totals[:player] == 21)
-        break if busted?(current_hands_totals, :dealer) || (current_hands_totals[:dealer] >= 17)
+        break if busted?(current_hands_totals, :player) || (current_hands_totals[:player] == GOAL)
+        break if busted?(current_hands_totals, :dealer) || (current_hands_totals[:dealer] >= DEALER_HOLD_POINT)
         prompt "Dealer hits."
         sleep 1.0
         hit_me(current_hands, :dealer, deck_in_play)
@@ -162,7 +177,7 @@ loop do
         update_hands_values(current_hands, current_hands_totals)
       end
 
-      if !busted?(current_hands_totals, :dealer) && !busted?(current_hands_totals, :player) && !(current_hands_totals[:player] == 21)
+      if !busted?(current_hands_totals, :dealer) && !busted?(current_hands_totals, :player) && !(current_hands_totals[:player] == GOAL)
         prompt "Dealer stays."
         sleep 1.0
       end
@@ -170,18 +185,17 @@ loop do
       display_hand_outcome(current_hands, current_hand_outcome, current_hands_totals)
       increment_scores(current_hand_outcome, scores)
       break if scores[:dealer] == 5 || scores[:player] == 5
-      # add display scores after victory; without doing so there is no apparent resoution before looping into 'rematch question'
     end
-    break if scores[:dealer] == 5 || scores[:player] == 5
-  end
-  prompt "Would you like a rematch?"
-  loop do
-    response = gets.chomp
-    break if response.casecmp('yes') == 0 || response.casecmp('no') == 0
-    prompt "That's not an option.  Please enter 'yes' or 'no'."
+    display_game_outcome(scores)
+    prompt "Would you like a rematch?"
+    loop do
+      response = gets.chomp
+      break if response.casecmp('yes') == 0 || response.casecmp('no') == 0
+      prompt "That's not an option.  Please enter 'yes' or 'no'."
+    end
+    break if response.casecmp('no') == 0
   end
   break if response.casecmp('no') == 0
-
 end
 
 prompt "Thanks for playing.  Good bye!"
--- crs101/lesson4/method_test.rb
+++ crs101/lesson4/method_test.rb
@@ -2,18 +2,32 @@ require 'pry'
 
 DECK = { '2 of S' => 2, '3 of S' => 3, '4 of S' => 4, '5 of S' => 5, '6 of S' => 6, '7 of S' => 7, '8 of S' => 8, '9 of S' => 9, '10 of S' => 10, 'Jack of S' => 10, 'Queen of S' => 10, 'King of S' => 10, 'Ace of S' => 11, '2 of Diamonds' => 2, '3 of Diamonds' => 3, '4 of Diamonds' => 4, '5 of Diamonds' => 5, '6 of Diamonds' => 6, '7 of Diamonds' => 7, '8 of Diamonds' => 8, '9 of Diamonds' => 9, '10 of Diamonds' => 10, 'Jack of Diamonds' => 10, 'Queen of Diamonds' => 10, 'King of Diamonds' => 10, 'Ace of Diamonds' => 11, '2 of Spades' => 2, '3 of Spades' => 3, '4 of Spades' => 4, '5 of Spades' => 5, '6 of Spades' => 6, '7 of Spades' => 7, '8 of Spades' => 8, '9 of Spades' => 9, '10 of Spades' => 10, 'Jack of Spades' => 10, 'Queen of Spades' => 10, 'King of Spades' => 10, 'Ace of Spades' => 11, '2 of Clubs' => 2, '3 of Clubs' => 3, '4 of Clubs' => 4, '5 of Clubs' => 5, '6 of Clubs' => 6, '7 of Clubs' => 7, '8 of Clubs' => 8, '9 of Clubs' => 9, '10 of Clubs' => 10, 'Jack of Clubs' => 10, 'Queen of Clubs' => 10, 'King of Clubs' => 10, 'Ace of Clubs' => 11 }
 #
-def shuffle_new_deck(source)
-  deck = source.to_a
-  deck.shuffle
+GOAL = 21
+DEALER_HOLD_POINT = 17
+scores = {dealer: 1}
+# current_hands = { dealer: [['King of S', 1, 'Ace of S' => 11,], player: [] }
+current_hands_totals = { dealer: 0, player: 0 }
+hearts = []
+diamonds = []
+clubs = []
+spades = []
+cards = [ ['2 of ', 2], ['3 of ', 3], ['4 of ', 4] ]
+suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
+deck = []
+
+# suits.each do |s|
+#   deck.push cards.each { |arr| arr[0].insert(-1, s) }
+#   binding.pry
+# end
+
+cards.each do |arr|
+  hearts.push arr[0].insert(-1, 'Hearts')
+  diamonds.push arr[0].insert(-1, 'Diamonds')
+
 end
 
-# current_hands = { dealer: [], player: [] }
-# deck_in_play = shuffle_new_deck(DECK)
-# deal_hands(deck_in_play, current_hands)
-# p current_hands
-
-loop do
-  puts "Would you like to hit or stay?"
-  response = gets.chomp
-  break if response.casecmp('stay') == 0
-end
+p deck
+p hearts
+p diamonds
+p clubs
+p spades
