968724e929c1a4d2999b041d85a76a16bc9656c3
jkurthoconnor <jkurthoconnor@gmail.com>
2016-08-25 08:43:22 -0600

    reduce logic complexity

--- crs120/lesson2/rps_oop.rb
+++ crs120/lesson2/rps_oop.rb
@@ -2,58 +2,43 @@
 
 class Move
   VALUES = ['rock', 'paper', 'scissors']
-  
+
   def initialize(value)
     @value = value
   end
-  
+
   def rock?
     @value == 'rock'
   end
-  
+
   def paper?
     @value == 'paper'
   end
-  
+
   def scissors?
     @value == 'scissors'
   end
-  
+
   def >(other_move)
-    if rock?
-      return true if other_move.scissors?
-      return false 
-    elsif paper?
-      return true if other_move.rock?
-      return false
-    elsif scissors?
-      return true if other_move.paper?
-      return false
-    end
+    (rock? && other_move.scissors?) ||
+      (paper? && other_move.rock?) ||
+      (scissors? && other_move.paper?)
   end
-    
+
   def <(other_move)
-    if rock?
-      return true if other_move.paper?
-      return false 
-    elsif paper?
-      return true if other_move.scissors?
-      return false
-    elsif scissors?
-      return true if other_move.rock?
-      return false
-    end
+    (rock? && other_move.paper?) ||
+      (paper? && other_move.scissors?) ||
+      (scissors? && other_move.rock?)
   end
-  
+
   def to_s
     @value
   end
 end
 
-
 class Player
   attr_accessor :move, :name
-  
+
   def initialize
     set_name
   end
@@ -68,9 +53,9 @@ class Human < Player
       break unless n.empty?
       puts "You must enter a name."
     end
-    self.name = n  
+    self.name = n
   end
-  
+
   def choose
     choice = nil
     loop do
@@ -83,90 +68,67 @@ class Human < Player
   end
 end
 
-class Computer < Player  
+class Computer < Player
   def set_name
-    self.name = ['Data', 'C3PO', 'BB8', 'R2D2'].sample  
+    self.name = ['Data', 'C3PO', 'BB8', 'R2D2'].sample
   end
-  
+
   def choose
-    self.move = Move.new(Move::VALUES.sample)    
+    self.move = Move.new(Move::VALUES.sample)
   end
-end
-
-
-class Rule
-  def initialize
-    # what is the state of a rule object?
-  end
-end
-
-# where should 'compare' go?
-def compare(move1, move2)
-  
 end
 
 class RPSGame
   attr_accessor :human, :computer
-  
+
   def initialize
-    @human = Human.new 
+    @human = Human.new
     @computer = Computer.new
   end
-  
+
   def display_welcome
-    puts "Welcome to Rock, Paper, Scissors.  Good luck, #{human.name}.  (You'll need it.)"
+    puts "Welcome to Rock, Paper, Scissors.  Good luck, #{human.name}.
+      (You'll need it.)"
   end
-  
+
   def display_goodbye
     puts "Nice game, #{human.name}!  Good Bye."
   end
-  
-  def display_winner
+
+  def display_moves
     puts "#{human.name} chose #{human.move}."
     puts "#{computer.name} chose #{computer.move}."
-    
+  end
+
+  def display_winner
     if human.move > computer.move
       puts "You won!"
     elsif human.move < computer.move
       puts "You loose :-( "
-    else 
+    else
       puts "It's a tie."
     end
-    
-    # case human.move
-    # when 'rock'
-    #   puts "You won!" if computer.move == 'scissors'
-    #   puts "It's a tie." if computer.move == 'rock'
-    #   puts "You loose :-( " if computer.move == 'paper'
-    # when 'paper'
-    #   puts "You won!" if computer.move == 'rock'
-    #   puts "It's a tie." if computer.move == 'paper'
-    #   puts "You loose :-( " if computer.move == 'scissors'
-    # when 'scissors'
-    #   puts "You won!" if computer.move == 'paper'
-    #   puts "It's a tie." if computer.move == 'scissors'
-    #   puts "You loose :-( " if computer.move == 'rock'
-    # end
   end
-  
+
   def play_again?
     answer = nil
     loop do
       puts "Would you like a rematch?  Enter 'y' or 'n'."
-      answer = gets.chomp
-      break if ['y', 'n'].include?(answer.downcase)
+      answer = gets.chomp.downcase
+      break if ['y', 'n'].include?(answer)
       puts "'#{answer}' is an invalid entry."
     end
-    
+
     return true if answer == 'y'
-    return false
+    return false if answer == 'n'
   end
-  
+
   def play
     display_welcome
     loop do
       human.choose
       computer.choose
+      display_moves
       display_winner
       break unless play_again?
     end
