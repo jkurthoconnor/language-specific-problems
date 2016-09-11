d6491ece77e1c03050483243dcb81ebc0c9fae40
jkurthoconnor <jkurthoconnor@gmail.com>
2016-09-10 22:21:08 -0600

    add play again

--- crs120/lesson5/ttt_oop_spike.rb
+++ crs120/lesson5/ttt_oop_spike.rb
@@ -1,45 +1,33 @@
-# code based on Launch School 'walkthrough' of Tick Tack Toe
-
-# Nouns: board, player, square, grid
-# Verbs: play, mark
-
-# Board
-# Square
-# Player
-# - mark
-# - play
-require 'pry'
-
 class Board
   WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] +
                   [[1, 4, 7], [2, 5, 8], [3, 6, 9]] +
                   [[1, 5, 9], [3, 5, 7]]
-  
+
   def initialize
     @squares = {}
     (1..9).each {|key| @squares[key] = Square.new}
   end
-  
+
   def get_square_at(key)
     @squares[key]
   end
-  
+
   def set_square_at(key, marker)
     @squares[key].marker = marker
   end
-  
+
   def unmarked_keys
     (1..9).select {|key| @squares[key].unmarked?}
   end
-  
+
   def full?
     unmarked_keys.length.zero?
   end
-  
+
   def someone_won?
     !!detect_winner
   end
-  #returns winning marker or nil
+
   def detect_winner
     WINNING_LINES.each do |line|
       line_marks = []
@@ -58,15 +46,15 @@ class Square
   INITIAL_MARKER = " "
 
   attr_accessor :marker
-  
+
   def initialize(marker=INITIAL_MARKER)
     @marker = marker
   end
-  
+
   def to_s
     @marker
   end
-  
+
   def unmarked?
     marker == INITIAL_MARKER
   end
@@ -74,7 +62,7 @@ end
 
 class Player
   attr_reader :marker
-  
+
   def initialize(marker)
     @marker = marker
   end
@@ -84,28 +72,29 @@ end
 class TTTGame
   HUMAN_MARKER = "X"
   COMPUTER_MARKER = "O"
-  
+
   attr_reader :board, :human, :computer
-  
+
   def initialize
     @board = Board.new
     @human = Player.new(HUMAN_MARKER)
     @computer = Player.new(COMPUTER_MARKER)
   end
-  
+
   def display_welcome_message
+    puts ""
     puts "Welcome to Tick, Tack, Toe."
     puts ""
-    puts "Your mark is #{HUMAN_MARKER} .  The computer's mark is #{COMPUTER_MARKER}"
-    sleep 2.0
   end
-  
+
   def display_goodbye_message
+    puts ""
     puts "Nice game.  Goodbye!"
   end
-  
-  def display_board
-    system 'clear' or system 'cls'
+
+  def display_board(clear_screen=true)
+    system 'clear' or system 'cls' unless !clear_screen
+    puts ""
     puts "You: '#{HUMAN_MARKER}'  || Computer: '#{COMPUTER_MARKER}'"
     puts ""
     puts ""
@@ -133,7 +122,7 @@ class TTTGame
     end
     board.set_square_at(square, human.marker)
   end
-  
+
   def computer_moves
     board.set_square_at(board.unmarked_keys.sample, computer.marker)
   end
@@ -141,26 +130,48 @@ class TTTGame
   def display_result
     display_board
     puts ""
-    puts "The board is full." if board.full?
+    puts "Looks like a tie." if board.full?
     puts "Congratulations. You won!" if board.detect_winner == HUMAN_MARKER
     puts "I'm sorry. You have lost." if board.detect_winner == COMPUTER_MARKER
   end
-  
+
+  def play_again?
+    puts ""
+    puts "Would you like to play again? (Enter 'y' or 'n'.)"
+    choice = gets.chomp
+    loop do
+      break if ['y', 'n'].include?(choice.downcase)
+      puts "#{choice} is not an option.  Please enter 'y' or 'n'."
+      choice = gets.chomp
+    end
+    return false if choice == 'n'
+    true
+  end
+
+  def reset_board
+    @board = Board.new
+  end
+
   def play
     display_welcome_message
-    display_board 
-
     loop do
-      human_moves
-      break if board.someone_won? || board.full?
+      display_board(false) 
 
-      computer_moves
-      break if board.someone_won? || board.full?
+      loop do
+        human_moves
+        break if board.someone_won? || board.full?
 
-      display_board
+        computer_moves
+        break if board.someone_won? || board.full?
+
+        display_board
+      end
+
+      display_result
+      break unless play_again?
+      puts "\nExcellent! Let's go..."
+      reset_board
     end
-
-    display_result
     display_goodbye_message
   end
 end
