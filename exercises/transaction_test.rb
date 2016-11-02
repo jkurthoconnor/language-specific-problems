87ab3857eee75f8ccee7da83e4be7c7ceacdf2dd
jkurthoconnor <jkurthoconnor@gmail.com>
2016-11-02 11:04:06 -0600

    completed test

--- exercises/transaction_test.rb
+++ exercises/transaction_test.rb
@@ -1,6 +1,6 @@
 require 'minitest/autorun'
-require "minitest/reporters"
-Minitest::Reporters.use!
+# require "minitest/reporters"
+# Minitest::Reporters.use!
 
 require_relative 'transaction'
 
@@ -23,13 +23,3 @@ class TransactionTest < Minitest::Test
     assert_equal(100, transaction.amount_paid)
   end
 end
-
-# def prompt_for_payment(input: $stdin) # We've set a default parameter for stdin
-#   loop do
-#     puts "You owe $#{item_cost}.\nHow much are you paying?"
-#     @amount_paid = input.gets.chomp.to_f # notice that we call gets on that parameter
-#     break if valid_payment? && sufficient_payment?
-#     puts 'That is not the correct amount. ' \
-#          'Please make sure to pay the full cost.'
-#   end
-# end
