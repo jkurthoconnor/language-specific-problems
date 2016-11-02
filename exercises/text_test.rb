71841d691d686377b134ff4995c635a74b5e9782
jkurthoconnor <jkurthoconnor@gmail.com>
2016-11-02 13:32:15 -0600

    add word_count test and confirmationmessages on file open and close

--- exercises/text_test.rb
+++ exercises/text_test.rb
@@ -6,6 +6,7 @@ class TextTest < MiniTest::Test
 
   def setup
     @file = File.open('sample2.txt', 'r')
+    puts "File is now open for reading: {#@file.open?}"
     @string =  @file.read
     @text = Text.new(string)
   end
@@ -15,7 +16,17 @@ class TextTest < MiniTest::Test
     assert_equal(expected_string, text.swap('a', 'e'))
   end
   
+  def test_word_count
+    expected_count = string.split.count
+    assert_equal(expected_count, text.word_count)
+  end
+
   def teardown
     @file.close
+    puts "File is now closed: #{@file.closed?}"
   end
 end
+
+# def word_count
+#   @text.split.count
+# end
