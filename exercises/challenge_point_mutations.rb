1acdf8ffaf01e54d3a029ec60b4264f8a1f83ab7
jkurthoconnor <jkurthoconnor@gmail.com>
2016-11-05 09:08:58 -0600

    refactor to allow strings of differing length

--- exercises/challenge_point_mutations.rb
+++ exercises/challenge_point_mutations.rb
@@ -1,25 +1,3 @@
-# Write a program that can calculate the Hamming difference between two DNA strands.
-# ...
-# 
-# The simplest and most common type of nucleic acid mutation is a point mutation, which replaces one base with another at a single nucleotide.
-# 
-# By counting the number of differences between two homologous DNA strands taken from different genomes with a common ancestor, we get a measure of the minimum number of point mutations that could have occurred on the evolutionary path between the two strands.
-# 
-# This is called the 'Hamming distance'
-# 
-# GAGCCTACTAACGGGAT
-# CATCGTAATGACGGCCT
-# ^ ^ ^  ^ ^    ^^
-# The Hamming distance between these two DNA strands is 7.
-# 
-# The Hamming distance is only defined for sequences of equal length. Hence you may assume that only sequences of equal length will be passed to your hamming distance function.
-
-# input: two upcased alpha strings of equal length; no spaces
-# 
-# count and return the number of points of difference
-  # point of difference: point (index?) at which the strings' content !=
-  # edge cases: when string length differs, ignore all points on longer string beyond shared length
-# output: integer representing the number of points of difference between strings
 
 class DNA
   def initialize(strand)
@@ -29,14 +7,27 @@ class DNA
   def hamming_distance(other_strand)
     ind = 0
     distance = 0
-    while ind < strand.length
+    end_point = calc_end_point(other_strand)
+
+    loop do
       distance += 1 if strand[ind] != other_strand[ind]
       ind += 1
+      break if ind >= end_point
     end
+
     distance
+  end
+  
+  def calc_end_point(other_strand)
+    case strand.length <=> other_strand.length
+    when 1 then other_strand.length
+    else        strand.length
+    end
   end
 
   protected
 
   attr_reader :strand
 end
+
+p DNA.new('').hamming_distance('')
