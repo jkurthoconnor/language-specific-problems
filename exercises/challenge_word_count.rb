3a30af954ceac2fb3cb5c3bc20c41b863ae06903
jkurthoconnor <jkurthoconnor@gmail.com>
2016-11-05 21:46:55 -0600

    init test suit and problem statement

---
+++ exercises/challenge_word_count.rb
@@ -0,0 +1,12 @@
+# Write a program that given a phrase can count the occurrences of each word in that phrase.
+# input: phrase string
+# output: hash with unique words from phrase as keys, ordered as they appear in phrase; values are corresponding word counts
+
+class Phrase
+  def initialize(string)
+    @phrase = string
+  end
+
+  def word_count
+  end
+end
