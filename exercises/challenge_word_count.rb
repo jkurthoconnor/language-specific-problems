92570329dd762c391c5827346d41d9063bae7316
jkurthoconnor <jkurthoconnor@gmail.com>
2016-11-05 21:56:17 -0600

    basic solution; pass initial tests

--- exercises/challenge_word_count.rb
+++ exercises/challenge_word_count.rb
@@ -3,10 +3,18 @@
 # output: hash with unique words from phrase as keys, ordered as they appear in phrase; values are corresponding word counts
 
 class Phrase
+  attr_accessor :phrase, :count
+
   def initialize(string)
-    @phrase = string
+    @phrase = string.split
+    @count = Hash.new(0)
   end
 
   def word_count
+    phrase.each { |word| count[word] += 1 }
+    count
   end
 end
+
+phrase = Phrase.new('word')
+phrase2 = Phrase.new('one of each')
