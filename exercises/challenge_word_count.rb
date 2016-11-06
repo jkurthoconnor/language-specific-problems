af8df42fff14b71bb7ebd4e7ad4ca8d08856e1cc
jkurthoconnor <jkurthoconnor@gmail.com>
2016-11-06 08:47:23 -0700

    refactor with #clean_string in place of hard-to-read @phrase initialization

--- exercises/challenge_word_count.rb
+++ exercises/challenge_word_count.rb
@@ -6,13 +6,18 @@ class Phrase
   attr_accessor :phrase, :count
 
   def initialize(string)
-    @phrase = string.downcase.gsub(',', ' ').delete("^ 'a-z0-9").split
+    @phrase = string
   end
 
   def word_count
     @count = Hash.new(0)
-    phrase.each { |word| count[word] += 1 }
+    words = clean_string
+    words.each { |word| count[word] += 1 }
     count
+  end
+
+  def clean_string
+    phrase.downcase.gsub(',', ' ').delete("^ 'a-z0-9").split
   end
 end
 
