ecbd053fe8a2f3d22358a5daff054deb420d3982
jkurthoconnor <jkurthoconnor@gmail.com>
2016-11-21 16:19:21 -0700

    add line to remove favicon request

--- crs170/echo_server.rb
+++ crs170/echo_server.rb
@@ -6,6 +6,7 @@ loop do
   client = server.accept
 
   request_line = client.gets
+  next if !request_line || request_line =~ /favicon/
   puts request_line # output to terminal
 
   client.puts request_line # output to client
