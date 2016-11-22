f2bcf030714b85b6ace2e3fb7e8b22bc73c98a0e
jkurthoconnor <jkurthoconnor@gmail.com>
2016-11-22 11:01:06 -0700

    add initial request parsing

--- crs170/parsing_server.rb
+++ crs170/parsing_server.rb
@@ -8,6 +8,19 @@ loop do
   request_line = client.gets
   next if !request_line || request_line =~ /favicon/
   puts request_line # output to terminal
+  
+  chunks = request_line.split[0, 2]
+  http_method, path_and_q = chunks[0], chunks[1]
+  
+  path_and_params = path_and_q.split('?')
+  path, param_str = path_and_params[0], path_and_params[1]
+  
+  params = {}
+  param_str.split('&').each do |query|
+    pair = query.split('=')
+    key, value = pair[0], pair[1]
+    params[key] = value
+  end
 
   client.puts request_line # output to client
   client.puts rand(6) + 1
