ecc4b9b8d5b2eb80ee138932e526c8620bb5c268
jkurthoconnor <jkurthoconnor@gmail.com>
2016-11-23 23:07:01 -0700

    implement chapter1 page and page styling

--- crs170/book_viewer/book_viewer.rb
+++ crs170/book_viewer/book_viewer.rb
@@ -8,3 +8,10 @@ get "/" do
 
   erb :home
 end
+
+get "/chapters/1" do
+  @title = "Chapter 1"
+  @toc = File.readlines("data/toc.txt")
+  @chapter1 = File.readlines("data/chp1.txt", "\n\n")
+  erb :chp1
+end
