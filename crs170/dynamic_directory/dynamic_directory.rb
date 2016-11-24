c8834b9ffbd3eb8d6a286b88669d56d1c53b5d45
jkurthoconnor <jkurthoconnor@gmail.com>
2016-11-24 14:56:00 -0700

    implement reverse list url query option

--- crs170/dynamic_directory/dynamic_directory.rb
+++ crs170/dynamic_directory/dynamic_directory.rb
@@ -2,8 +2,14 @@ require "tilt/erubis"
 require "sinatra"
 require "sinatra/reloader"
 
-get "/" do 
-  @paths_files = Dir.glob("public/*")
+get "/" do
+  path = request.fullpath
+  option = path.split('?').last.split('=').last
+  if option == 'reverse'
+    @paths_files = Dir.glob("public/*").reverse
+  else
+    @paths_files = Dir.glob("public/*")
+  end
 
   erb :home
 end
