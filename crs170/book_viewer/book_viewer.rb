939cbb10ba6f53133eba865ae8a955337a48a9c6
jkurthoconnor <jkurthoconnor@gmail.com>
2016-11-24 22:25:07 -0700

    add helper method to divide paragraphs in chapers; replaces File.readlines used in assigning @chapter value

--- crs170/book_viewer/book_viewer.rb
+++ crs170/book_viewer/book_viewer.rb
@@ -2,18 +2,27 @@ require "tilt/erubis"
 require "sinatra"
 require "sinatra/reloader"
 
+before do
+  @toc = File.readlines("data/toc.txt")
+end
+
+helpers do
+  def in_paragraphs(text)
+    paragraphs = text.split("\n\n")
+    paragraphs.map { |para| "<p>#{para}</p>" }.join
+  end
+end
+
 get "/" do
   @title = "The Adventures of Sherlock Holmes"
-  @toc = File.readlines("data/toc.txt")
 
   erb :home
 end
 
 get "/chapters/:number" do
   number = params[:number]
-  @toc = File.readlines("data/toc.txt")
   @title = @toc[number.to_i - 1]
-  @chapter = File.readlines("data/chp#{number}.txt", "\n\n")
+  @chapter = File.read("data/chp#{number}.txt")
 
   erb :chapter
 end
