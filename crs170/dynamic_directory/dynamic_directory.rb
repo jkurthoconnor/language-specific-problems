a87b971eb090763b19faa38d3ee73b8962cda9c9
jkurthoconnor <jkurthoconnor@gmail.com>
2016-11-24 19:04:42 -0700

    add reverse order sorting logic and link

--- crs170/dynamic_directory/dynamic_directory.rb
+++ crs170/dynamic_directory/dynamic_directory.rb
@@ -1,14 +1,14 @@
-require "tilt/erubis"
-require "sinatra"
-require "sinatra/reloader"
+require 'tilt/erubis'
+require 'sinatra'
+require 'sinatra/reloader'
 
-get "/" do
+get '/' do
   path = request.fullpath
-  option = path.split('?').last.split('=').last
+  option = path.split('=').last
   if option == 'reverse'
-    @paths_files = Dir.glob("public/*").reverse
+    @paths_files = Dir.glob('public/*').reverse
   else
-    @paths_files = Dir.glob("public/*")
+    @paths_files = Dir.glob('public/*')
   end
 
   erb :home
