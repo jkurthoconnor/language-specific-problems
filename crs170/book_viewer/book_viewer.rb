d4cfc6e0b33ad19863df509838912ad14a571457
jkurthoconnor <jkurthoconnor@gmail.com>
2016-11-25 11:31:24 -0700

    implement chapter search function returning links

--- crs170/book_viewer/book_viewer.rb
+++ crs170/book_viewer/book_viewer.rb
@@ -11,6 +11,20 @@ helpers do
     paragraphs = text.split("\n\n")
     paragraphs.map { |para| "<p>#{para}</p>" }.join
   end
+  
+  def show_search_message(hits)
+    # show results heading and list if hits
+    # show no matches message if no hits
+    # show no message if no search terms entered
+  end
+
+  def list_search_results
+    links = @hits.map do |hit| 
+      "<li><a href='/chapters/#{hit}'>#{@toc[hit - 1]}</a></li>"
+    end
+
+    links.join
+  end
 end
 
 get "/" do
@@ -26,6 +40,21 @@ get "/chapters/:number" do
   @chapter = File.read("data/chp#{number}.txt")
 
   erb :chapter
+end
+
+get "/search" do
+  @chapter = File.read("data/chp1.txt")
+  @term = params[:query]
+  @hits = []
+
+  if @term
+    @toc.size.times do |n|
+      chapter = File.read("data/chp#{n + 1}.txt")
+      @hits.push(n + 1) if chapter.include?(@term)
+    end
+  end
+
+  erb :search
 end
 
 not_found do
