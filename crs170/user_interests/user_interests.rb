91b2eb68e3f5f8edd33e78e925c1d13597fbc410
jkurthoconnor <jkurthoconnor@gmail.com>
2016-11-26 11:58:19 -0700

    add user pages; clarify naming convention

--- crs170/user_interests/user_interests.rb
+++ crs170/user_interests/user_interests.rb
@@ -4,13 +4,33 @@ require 'sinatra/reloader'
 require 'yaml'
 
 before do
-  @users = YAML.load_file('users.yaml')
+  @data = YAML.load_file('users.yaml')
+  # {:jamy=>{:email=>"jamy.rustenburg@gmail.com", :interests=>["woodworking", "cooking", "reading"]},   :nora=>{:email=>"nora.alnes@yahoo.com", :interests=>["cycling", "basketball", "economics"]}, :hiroko=>{:email=>"hiroko.ohara@hotmail.com", :interests=>["politics", "history", "birding"]}}
 end
 
 get '/' do
-  redirect '/users'
+  redirect '/users/list'
 end 
 
 get '/users' do
-  erb :users
+  redirect '/users/list'
 end
+
+get '/users/list' do
+  erb :name_list
+end
+
+get '/users/:name' do
+  redirect 'users/list' unless @data.keys.include?(params[:name].to_sym)
+
+  @user_name = params[:name].to_sym
+  @user_profile = @data[@user_name]
+
+  erb :user_page
+end
+
+not_found do
+  redirect 'users/list'
+end
+  
+  
