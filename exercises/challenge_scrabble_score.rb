d139c66f5760ca60da34953a6222f1862aea7020
jkurthoconnor <jkurthoconnor@gmail.com>
2016-11-28 09:39:59 -0700

    init new challenge

---
+++ exercises/challenge_scrabble_score.rb
@@ -0,0 +1,17 @@
+# Write a program that, given a word, computes the scrabble score for that word.
+# 
+# Letter Values
+#  
+# Letter                           Value
+# A, E, I, O, U, L, N, R, S, T       1
+# D, G                               2
+# B, C, M, P                         3
+# F, H, V, W, Y                      4
+# K                                  5
+# J, X                               8
+# Q, Z                               10
+#
+# Examples
+# 
+# "cabbage" should be scored as worth 14 points:
+
