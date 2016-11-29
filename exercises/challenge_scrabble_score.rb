0a7426730e967f49c31d40c1bfb14a15974155ef
jkurthoconnor <jkurthoconnor@gmail.com>
2016-11-28 22:33:54 -0700

    refactor as a map...reduce problem

--- exercises/challenge_scrabble_score.rb
+++ exercises/challenge_scrabble_score.rb
@@ -1,7 +1,4 @@
 # Write a program that, given a word, computes the scrabble score for that word.
-# 
-# Letter Values
-#  
 # Letter                           Value
 # A, E, I, O, U, L, N, R, S, T       1
 # D, G                               2
@@ -13,44 +10,27 @@
 #
 
 class Scrabble
+  POINT_VALUE = { 'A' => 1, 'E' => 1, 'I' => 1, 'O' => 1, 'U' => 1, 'L' => 1,
+                  'N' => 1, 'R' => 1, 'S' => 1, 'T' => 1, 'D' => 2, 'G' => 2,
+                  'B' => 3, 'C' => 3, 'M' => 3, 'P' => 3, 'F' => 4, 'H' => 4,
+                  'V' => 4, 'W' => 4, 'Y' => 4, 'K' => 5, 'J' => 8, 'X' => 8,
+                  'Q' => 10, 'Z' => 10 }.freeze
+
   def initialize(string)
     @string = string
-    @points = 0
   end
 
   def valid_input?
-     @string.is_a?(String) &&
-     @string =~ /\A[a-z]+\z/i
+    @string.is_a?(String) &&
+      @string =~ /\A[a-z]+\z/i
   end
 
   def score
-    return @points unless valid_input?
-    @string.upcase.each_char do |char|
-      @points += look_up_points(char)
-    end
-
-    @points
+    return 0 unless valid_input?
+    @string.upcase.chars.map { |char| POINT_VALUE[char] }.reduce(&:+)
   end
-  
+
   def self.score(string)
     Scrabble.new(string).score
-  end
-
-  def look_up_points(char)
-    if ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'].include?(char)
-      1
-    elsif ['D', 'G'].include?(char)
-      2
-    elsif ['B', 'C', 'M', 'P'].include?(char)
-      3
-    elsif ['F', 'H', 'V', 'W', 'Y'].include?(char)
-      4
-    elsif ['K'].include?(char)
-      5
-    elsif ['J', 'X'].include?(char)
-      8
-    elsif ['Q', 'Z'].include?(char)
-      10
-    end
   end
 end
