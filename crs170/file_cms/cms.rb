ea8d30d375ba593d36be773f62e4125cbd12a4dd
jkurthoconnor <jkurthoconnor@gmail.com>
2016-12-09 20:55:16 -0700

    add index and filename list

--- crs170/file_cms/cms.rb
+++ crs170/file_cms/cms.rb
@@ -1,4 +1,16 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require 'sinatra/content_for'
-require 'tilt/erubus'
+require 'tilt/erubis'
+
+helpers
+
+def clean_filename(name)
+  name.split('/').last
+end
+
+get '/' do
+  @files = Dir.glob("public/*").select { |filename| filename =~ /\./ }
+
+  erb :index
+end
