9ce50f49af02e12a64dbbccce51e1d70d334a422
jkurthoconnor <jkurthoconnor@gmail.com>
2016-12-11 19:44:43 -0700

    establish tests for all current routes

--- crs170/file_cms/test/cms_test.rb
+++ crs170/file_cms/test/cms_test.rb
@@ -1,9 +1,9 @@
-ENV["RACK_ENV"] = "test"
+ENV['RACK_ENV'] = 'test'
 
-require "minitest/autorun"
-require "rack/test"
+require 'minitest/autorun'
+require 'rack/test'
 
-require_relative "../cms"
+require_relative '../cms'
 
 class AppTest < Minitest::Test
   include Rack::Test::Methods
@@ -13,9 +13,33 @@ class AppTest < Minitest::Test
   end
 
   def test_index
-    get "/"
-    assert_equal 200, last_response.status
-    assert_equal "text/html;charset=utf-8", last_response["Content-Type"]
-    # assert_equal "Hello, world!", last_response.body
+    get '/'
+    assert_equal(200, last_response.status)
+    assert_equal('text/html;charset=utf-8', last_response['Content-Type'])
+
+    assert_includes(last_response.body, 'about.txt')
+    assert_includes(last_response.body, 'changes.txt')
+    assert_includes(last_response.body, 'history.txt')
+  end
+
+  def test_about
+    get '/about.txt'
+    assert_equal(200, last_response.status)
+    assert_equal('text/plain', last_response['Content-Type'])
+    assert_includes(last_response.body, 'ABOUT RUBY')
+  end
+
+  def test_changes
+    get '/changes.txt'
+    assert_equal(200, last_response.status)
+    assert_equal('text/plain', last_response['Content-Type'])
+    assert_includes(last_response.body, 'RUBY CHANGES')
+  end
+
+  def test_history
+    get '/history.txt'
+    assert_equal(200, last_response.status)
+    assert_equal('text/plain', last_response['Content-Type'])
+    assert_includes(last_response.body, 'RUBY HISTORY')
   end
 end
