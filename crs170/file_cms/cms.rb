2dbb32d5ba01e1db5745150ac6cbf7a58bade856
jkurthoconnor <jkurthoconnor@gmail.com>
2016-12-14 10:00:17 -0700

    extract content loading into separate method; add assertion to / test

--- crs170/file_cms/cms.rb
+++ crs170/file_cms/cms.rb
@@ -25,29 +25,36 @@ get '/' do
 end
 
 
-def render_markdown(file)
+def render_markdown(text)
   markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
-  markdown.render(file)
+  markdown.render(text)
 end
+
+
+def load_content(resource)
+  content = File.read(resource)
+
+  case File.extname(resource)
+  when '.txt'
+    @text = content
+    headers['Content-Type'] = 'text/plain'  # sets content type in response header
+  when '.md'
+    @text = render_markdown(content)
+  end
+end
+
 
 get '/:filename' do
   redirect '/' if params[:filename] =~ /favicon/
 
-  resource = root + '/data/' + params[:filename]
+  path = root + '/data/' + params[:filename]
 
-  unless File.file?(resource)
+  unless File.file?(path)
   session[:error] = "#{params[:filename]} does not exist"
   redirect '/'
   end
 
-  file = File.read(resource)
+  load_content(path)
 
-  case File.extname(resource)
-  when '.txt'
-    @text = file
-    headers['Content-Type'] = 'text/plain'  # sets content type in response header
-  when '.md'
-    @text = render_markdown(file)
-  end
   erb :document
 end
--- crs170/file_cms/test/cms_test.rb
+++ crs170/file_cms/test/cms_test.rb
@@ -16,7 +16,7 @@ class AppTest < Minitest::Test
     get '/'
     assert_equal(200, last_response.status)
     assert_equal('text/html;charset=utf-8', last_response['Content-Type'])
-
+    assert_includes(last_response.body, 'about.md')
     assert_includes(last_response.body, 'about.txt')
     assert_includes(last_response.body, 'changes.txt')
     assert_includes(last_response.body, 'history.txt')
