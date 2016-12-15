d833f7d37c91c764f19d89915d9497c9f45cfbb1
jkurthoconnor <jkurthoconnor@gmail.com>
2016-12-15 10:27:22 -0700

    refactor tests to employ test data

--- crs170/file_cms/test/cms_test.rb
+++ crs170/file_cms/test/cms_test.rb
@@ -27,7 +27,6 @@ class AppTest < Minitest::Test
     create_document('about.md')
     create_document('changes.txt')
 
-
     get '/'
 
     assert_equal(200, last_response.status)
@@ -36,86 +35,80 @@ class AppTest < Minitest::Test
     assert_includes(last_response.body, 'changes.txt')
   end
 
-  # def test_about
-  #   get '/about.txt'
-  #   assert_equal(200, last_response.status)
-  #   assert_equal('text/plain', last_response['Content-Type'])
-  #   assert_includes(last_response.body, 'ABOUT RUBY')
-  # end
-  #
-  # def test_changes
-  #   get '/changes.txt'
-  #   assert_equal(200, last_response.status)
-  #   assert_equal('text/plain', last_response['Content-Type'])
-  #   assert_includes(last_response.body, 'RUBY CHANGES')
-  # end
-  #
-  # def test_history
-  #   get '/history.txt'
-  #   assert_equal(200, last_response.status)
-  #   assert_equal('text/plain', last_response['Content-Type'])
-  #   assert_includes(last_response.body, 'RUBY HISTORY')
-  # end
-  #
-  # def test_nonexistent_doc
-  #   get '/nonexistent.doc'
-  #   assert_equal(302, last_response.status)
-  #
-  #   get last_response['Location']
-  #   assert_equal(200, last_response.status)
-  #   assert_includes(last_response.body, 'nonexistent.doc does not exist')
-  #
-  #   get '/'
-  #   refute_includes(last_response.body, 'does not exist')
-  # end
-  #
-  # def test_favicon_redirect
-  #   get '/favicon.ico'
-  #   assert_equal(302, last_response.status)
-  #
-  #   get last_response['Location']
-  #   assert_equal(200, last_response.status)
-  #   refute_includes(last_response.body, 'does not exist')
-  # end
-  #
-  # def test_markdown_file
-  #   get 'about.md'
-  #   assert_equal(200, last_response.status)
-  #   assert_equal('text/html;charset=utf-8', last_response['Content-Type'])
-  #   # assert_includes(last_response.body, '<h3>ABOUT RUBY</h3>')
-  # end
+  def test_text_document
+    create_document('about.txt', 'ABOUT RUBY')
 
-  # def test_edit_view
-  #   sample_text = "<textarea name=\"revised_text\" rows=\"20\" cols=\"80\">"
-  #
-  #   get 'about.md/edit'
-  #   assert_equal(200, last_response.status)
-  #   assert_includes(last_response.body, sample_text)
-  #
-  #   post 'about.md/edit', revised_text: '## changed'
-  #   assert_equal(302, last_response.status)
-  #
-  #   # # assertion fails: why?
-  #   # get last_response['Location']
-  #   # assert_includes(last_response.body, 'has been edited!')
-  #
-  #   get '/about.md'
-  #   assert_equal(200, last_response.status)
-  #   assert_includes(last_response.body, '<h2>changed</h2>')
-  #
-  #   get 'about.txt/edit'
-  #   assert_equal(200, last_response.status)
-  #   assert_includes(last_response.body, sample_text)
-  #
-  #   get 'changes.txt/edit'
-  #   assert_equal(200, last_response.status)
-  #   assert_includes(last_response.body, sample_text)
-  #
-  #   get 'history.txt/edit'
-  #   assert_equal(200, last_response.status)
-  #   assert_includes(last_response.body, sample_text)
-  #
-  # end
+    get '/about.txt'
+
+    assert_equal(200, last_response.status)
+    assert_equal('text/plain', last_response['Content-Type'])
+    assert_includes(last_response.body, 'ABOUT RUBY')
+  end
+
+  def test_nonexistent_doc
+    get '/nonexistent.doc'
+
+    assert_equal(302, last_response.status)
+
+    get last_response['Location']
+
+    assert_equal(200, last_response.status)
+    assert_includes(last_response.body, 'nonexistent.doc does not exist')
+
+    get '/'
+
+    refute_includes(last_response.body, 'does not exist')
+  end
+
+  def test_favicon_redirect
+    get '/favicon.ico'
+
+    assert_equal(302, last_response.status)
+
+    get last_response['Location']
+
+    assert_equal(200, last_response.status)
+    refute_includes(last_response.body, 'does not exist')
+  end
+
+  def test_markdown_file
+    create_document('about.md', '## Header')
+
+    get 'about.md'
+
+    assert_equal(200, last_response.status)
+    assert_equal('text/html;charset=utf-8', last_response['Content-Type'])
+    assert_includes(last_response.body, '<h2>Header</h2>')
+  end
+
+  def test_edit_view
+    sample_text = "<textarea name=\"revised_text\" rows=\"20\" cols=\"80\">"
+    create_document('about.md', sample_text)
+    create_document('changes.txt', sample_text)
+
+    get 'about.md/edit'
+
+    assert_equal(200, last_response.status)
+    assert_includes(last_response.body, sample_text)
+
+    post 'about.md/edit', revised_text: '## changed'
+
+    assert_equal(302, last_response.status)
+
+    # # assertion fails: why?
+    # get last_response['Location']
+    # assert_includes(last_response.body, 'has been edited!')
+
+    get '/about.md'
+
+    assert_equal(200, last_response.status)
+    assert_includes(last_response.body, '<h2>changed</h2>')
+
+    get 'changes.txt/edit'
+
+    assert_equal(200, last_response.status)
+    assert_includes(last_response.body, sample_text)
+  end
 
   def teardown
     FileUtils.rm_rf(data_path)
