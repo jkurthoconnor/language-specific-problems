0ccce5f2d1e4b3232aaa0272a75515a6e76fe69e
jkurthoconnor <jkurthoconnor@gmail.com>
2016-12-23 22:35:06 -0700

    add links for next question, change category; add headings

--- drill_app/fluency_drills.rb
+++ drill_app/fluency_drills.rb
@@ -3,43 +3,61 @@ require 'sinatra/reloader'
 require 'tilt/erubis'
 require 'yaml'
 
-helpers do
-  def isolate_name(file_path)
-    File.basename(file_path, '.*')
-  end
-end
+require 'pry'
 
 
 data_path = File.expand_path('../data', __FILE__)
 
-# displays list of available question types
-get '/' do
+helpers do
+  def isolate_name(file_path)
+    File.basename(file_path, '.*')
+  end
+  
+  def list_other_categories(paths)
+    # binding.pry
+    others = paths.reject { |type| type.include?(params[:type]) }
+    others.map { |type| isolate_name(type) }
+  end
+end
+
+before do
   pattern = File.join(data_path, '*')
   @file_paths = Dir.glob(pattern)
+end
 
+
+# displays list of available question types
+get '/' do
+  
   erb :index
 end
 
 
 # displays a question of the given type
-get '/quiz/:questiontype' do
-  file = params[:questiontype] + '.yml'
+get '/question/:type' do
+  file = params[:type] + '.yml'
   file_path = File.join(data_path, file)
   question_set = YAML.load_file(file_path)
-  @question = question_set[rand(question_set.size)]
+  @drill = question_set[rand(question_set.size)]
 
   erb :quiz
 end
 
+
+# styling
+  # add title header to layout, banner style
+  # throughout use buttons (actual form buttons || links styled as buttons)
+  # use a separator to demarkate lines / subquestions in yaml files;
+  #    - introduce some formatting/parsing to style
+  # 
 # Requirements
-# 1. menu page
-#   - list of available question types
-#   - selecting (?button, link?) a type takes to question page
-#   
-# 2. question page
-#   - user presented with a question of selected type
-#   - given options: 
-#     - see question again soon
-#     - next question
-#     - menu
-#     
+# 
+# question page
+#   - user is given options: 
+#     - rank question difficulty
+#    
+# add an edit page
+    # - allow editing of questions
+    # - allow adding new questions
+    
+# improve index page (or remove it?)
