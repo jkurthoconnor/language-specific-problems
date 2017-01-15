693981d0b358d494183bc606298400bf449e3980
jkurthoconnor <jkurthoconnor@gmail.com>
2017-01-15 15:08:01 -0700

    add file indexing

--- crs170/file_cms_2/file_cms_2.rb
+++ crs170/file_cms_2/file_cms_2.rb
@@ -2,6 +2,19 @@ require 'sinatra'
 require 'sinatra/reloader' if development?
 require 'tilt/erubis'
 
+
+root = File.expand_path('..', __FILE__)
+data_path = File.join(root, 'data')
+
+helpers do
+  def shortname(full_filename)
+    File.basename(full_filename)
+  end
+end
+
 get '/' do
-  'Getting started.'
+  file_paths = File.join(data_path, '*')
+  @files = Dir.glob(file_paths)
+
+  erb :index
 end
