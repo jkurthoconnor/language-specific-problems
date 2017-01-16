001e703f3d75b1d45f788d87271c8d06955f5879
jkurthoconnor <jkurthoconnor@gmail.com>
2017-01-16 13:52:12 -0700

    add message and redirection for non-existant files; add tests

--- crs170/file_cms_2/cms2.rb
+++ crs170/file_cms_2/cms2.rb
@@ -2,6 +2,10 @@ require 'sinatra'
 require 'sinatra/reloader' if development?
 require 'tilt/erubis'
 
+configure do
+  enable :sessions
+  set :session_secret, 'ha'
+end
 
 root = File.expand_path('..', __FILE__)
 data_path = File.join(root, 'data')
@@ -12,16 +16,29 @@ helpers do
   end
 end
 
-# view index page
-get '/' do
+before do
   file_paths = File.join(data_path, '*')
   @files = Dir.glob(file_paths)
+end
 
+
+# view index page
+get '/' do
   erb :index
+end
+
+def valid_filename?(name)
+  filenames = @files.map {|file| File.basename(file)}
+  filenames.include?(name)
 end
 
 # view individual document page
 get '/:filename' do
+  unless valid_filename?(params[:filename])
+    session[:message] = "#{params[:filename]} does not exist."
+    redirect '/'
+  end
+
   file_path = File.join(data_path, params[:filename])
   file = File.read(file_path)
 
--- crs170/file_cms_2/test/cms2_test.rb
+++ crs170/file_cms_2/test/cms2_test.rb
@@ -21,4 +21,23 @@ class CmsTest < Minitest::Test
     assert_includes(last_response.body, 'changes.txt</a></li>')
     assert_includes(last_response.body, 'history.txt</a></li>')
   end
+
+  def test_text_docs
+    get '/about.txt'
+
+    assert_equal(200, last_response.status)
+    assert_equal('text/plain', last_response['Content-Type'])
+    assert_includes(last_response.body, 'ABOUT')
+  end
+
+  def test_nonexistent_doc
+    get '/idontexist.txt'
+    assert_equal(302, last_response.status)
+
+    get '/'
+    assert_includes(last_response.body, 'idontexist.txt does not exist.')
+
+    get '/'
+    refute_includes(last_response.body, 'idontexist.txt does not exist.')
+  end
 end
