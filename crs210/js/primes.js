3bbd8ad2d3f632ad124d9ecf5338f44960a52683
jkurthoconnor <jkurthoconnor@gmail.com>
2017-12-21 21:12:34 -0700

    revise for condition

--- crs210/js/primes.js
+++ crs210/js/primes.js
@@ -7,7 +7,7 @@ function isPrime(number) {
     return true;
   }
 
-  for (i = 2; i < number / 2; i++) {
+  for (i = 2; i <= number / 2; i++) {
     if (number % i === 0) {
       return false;
     }
@@ -18,6 +18,7 @@ function isPrime(number) {
 console.log(isPrime(1));
 console.log(isPrime(2));
 console.log(isPrime(3));
+console.log(isPrime(4));
 console.log(isPrime(43));
 console.log(isPrime(55));
 console.log(isPrime(0));
