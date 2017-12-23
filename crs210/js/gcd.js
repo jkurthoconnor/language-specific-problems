90160750fdf9ef7e38263563e7d0c45bd47aa7b9
jkurthoconnor <jkurthoconnor@gmail.com>
2017-12-23 13:26:39 -0700

    complete working solution

--- crs210/js/gcd.js
+++ crs210/js/gcd.js
@@ -1,21 +1,28 @@
+function factor(integer) {
+  var i;
+  var factors = [];
+
+  for (i = integer; i >= 1; i--) {
+    if (integer % i === 0) {
+      factors.push(i);
+    }
+  }
+  return factors;
+}
+
 function gcd(int1, int2) {
   var i;
-  var factors1 = [];
-  var factors2 = [];
+  var j;
+  var factors1 = factor(int1);
+  var factors2 = factor(int2);
 
-  for (i = int1; i >= 1; i--) {
-    if (int1 % i === 0) {
-      factors1.push(i);
+  for (i = 0; i < factors1.length; i++) {
+    for (j = 0; j < factors2.length; j++) {
+      if (factors1[i] === factors2[j]) {
+        return factors1[i];
+      }
     }
   }
-
-  for (i = int2; i >= 1; i--) {
-    if (int2 % i === 0) {
-      factors2.push(i);
-    }
-  }
-
-  return [factors1, factors2];
 }
 
 console.log(gcd(12, 4));
