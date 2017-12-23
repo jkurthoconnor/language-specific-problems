b1e190e04334373f79f85e4e5e835087a546c899
jkurthoconnor <jkurthoconnor@gmail.com>
2017-12-23 16:20:53 -0700

    spike: goldbach identification functional, output not formatted

---
+++ crs210/js/goldbach.js
@@ -0,0 +1,38 @@
+function isPrime(number) {
+  var i;
+
+  if (number < 2) {
+    return false;
+  }
+
+  for (i = 2; i <= number / 2; i++) {
+    if (number % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function checkGoldbach(n) {
+  var result = [];
+  var x;
+
+  if (n % 2 !== 0 || n < 4) {
+    result = null;
+  }
+
+  for (i = 2; i <= n / 2; i++) {
+    x = n - i;
+    if (isPrime(x) && isPrime(i)) {
+      result.push([i, x]);
+    }
+  }
+  console.log(result);
+}
+
+checkGoldbach(1);
+checkGoldbach(2);
+checkGoldbach(3);
+checkGoldbach(4);
+checkGoldbach(12);
+checkGoldbach(100);
