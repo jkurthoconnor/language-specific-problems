bc025d359b4c6ed1c548cc34e11c40e82b527904
jkurthoconnor <jkurthoconnor@gmail.com>
2017-12-30 21:39:16 -0700

    spike: logic works with array

--- crs210/js/pattern.js
+++ crs210/js/pattern.js
@@ -1,6 +1,33 @@
 function generatePattern(n) {
+  var line;
+  var i;
+  var j;
+  var k;
+
+  for (i = 1; i <= n; i++) {
+    line = [];
+
+    for (j = 1; j <= i; j++) {
+      line.push(String(j));
+    }
+
+    for (k = 1; ; k++) {
+      if (j + k > n + 1) {
+        break;
+      }
+      line.push('*');
+    }
+
+    console.log(String(line));
+  }
+
  // log n rows of n chars
  // where each row begins
  // with 1..n, and the remaining
  // chars are *
 }
+
+generatePattern(1);
+generatePattern(4);
+generatePattern(8);
+generatePattern(10);
