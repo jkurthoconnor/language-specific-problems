d86fe235f37f6567c78392df5cfbed23e1b77f06
jkurthoconnor <jkurthoconnor@gmail.com>
2018-01-01 15:17:58 -0700

    implement second function

--- crs210/js/indexOfSub.js
+++ crs210/js/indexOfSub.js
@@ -26,7 +26,30 @@ function indexOf(firstString, secondString) {
 }
 
 function lastIndexOf(firstString, secondString) {
+  var i;
+  var j;
+  var sourceIdx;
+  var viableLength = firstString.length - secondString.length;
 
+  for (i = viableLength; i >= 0; i--) {
+    sourceIdx = 0;
+
+    if (firstString[i] === secondString[sourceIdx]) {
+
+      for (j = i + 1; ; j++) {
+        if (sourceIdx >= secondString.length - 1) {
+          return i;
+        }
+
+        sourceIdx++;
+
+        if (firstString[j] !== secondString[sourceIdx]) {
+          break;
+        }
+      } 
+    }
+  }
+  return -1;
 }
 
 console.log(indexOf('Some strings', 's'));                      // 5
@@ -35,6 +58,6 @@ console.log(indexOf('Blue Whale', 'Blute'));                    // -1
 console.log(indexOf('Blue Whale', 'leB'));                      // -1
 console.log(indexOf('Blue Whale', '^'));                      // -1
 
-// lastIndexOf('Some strings', 's');                  // 11
-// lastIndexOf('Blue Whale, Killer Whale', 'Whale');  // 19
-// lastIndexOf('Blue Whale, Killer Whale', 'all');    // -1
+console.log(lastIndexOf('Some strings', 's'));                  // 11
+console.log(lastIndexOf('Blue Whale, Killer Whale', 'Whale'));  // 19
+console.log(lastIndexOf('Blue Whale, Killer Whale', 'all'));    // -1
