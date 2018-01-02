c336e9fe4c2f8adc33bc6d03a402d3d646c79aed
jkurthoconnor <jkurthoconnor@gmail.com>
2018-01-02 12:19:10 -0700

    function functional

---
+++ crs210/js/stringStartsWith.js
@@ -0,0 +1,20 @@
+function startsWith(string, searchString) {
+  var i;
+
+  for (i = 0; i < searchString.length; i++) {
+    if (string[i] !== searchString[i]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+var str = 'We put comprehension and mastery above all else';
+console.log(startsWith(str, 'We'));              // true
+console.log(startsWith(str, 'We put'));          // true
+console.log(startsWith(str, ''));                // true
+console.log(startsWith(str, 'put'));             // false
+
+var longerString = 'We put comprehension and mastery above all else!';
+console.log(startsWith(str, longerString));      // false
