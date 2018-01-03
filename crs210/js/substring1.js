1ab2c540189fd014c0ad5eb98733123593e9902b
jkurthoconnor <jkurthoconnor@gmail.com>
2018-01-02 20:25:31 -0700

    all test cases satisfied

--- crs210/js/substring1.js
+++ crs210/js/substring1.js
@@ -1,13 +1,27 @@
 function substr(string, start, length) {
+  var i;
+  var subString = '';
+  var startIdx;
+  var stopIdx;
 
+  startIdx = start >= 0 ? start : string.length + start;
+  stopIdx = (length + startIdx) - 1;
 
+  for (i = startIdx; i <= stopIdx; i++) {
+    if (i >= string.length) {
+      break;
+    }
+    subString += string[i];
+  }
+
+  return subString;
 }
 
 
 var string = 'hello world';
 
-substr(string, 2, 4);      // "llo "
-substr(string, -3, 2);     // "rl"
-substr(string, 8, 20);     // "rld"
-substr(string, 0, -20);    // ""
-substr(string, 0, 0);      // ""
+console.log(substr(string, 2, 4));      // "llo "
+console.log(substr(string, -3, 2));     // "rl"
+console.log(substr(string, 8, 20));     // "rld"
+console.log(substr(string, 0, -20));    // ""
+console.log(substr(string, 0, 0));      // ""
