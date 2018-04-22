6d734359da9a6d605c1017bec21e1f33f17e6f45
jkurthoconnor <jkurthoconnor@gmail.com>
2018-04-21 21:19:07 -0600

    prevent version numbers from ending with dot

--- crs210/js/lesson9/compareVersions.js
+++ crs210/js/lesson9/compareVersions.js
@@ -1,33 +1,33 @@
 function compareVersions(ver1, ver2) {
-  if (ver1.match(/[^\d.]/) || ver2.match(/[^\d.]/)) {
+  if (ver1.match(/[^\d.]|\.$/) || ver2.match(/[^\d.]|\.$/)) {
     return null;
   }
 
-  var ver1Values = ver1.split('.').map(function (str) {
+  var values1 = ver1.split('.').map(function (str) {
     return parseInt(str, 10);
   });
-  var ver2Values = ver2.split('.').map(function (str) {
+  var values2 = ver2.split('.').map(function (str) {
     return parseInt(str, 10);
   });
 
-  removeTrailing0s(ver1Values);
-  removeTrailing0s(ver2Values);
+  removeTrailing0s(values1);
+  removeTrailing0s(values2);
 
-  for (var i = 0; i < ver1Values.length; i += 1) {
-    if (!ver2Values[i]) {
+  for (var i = 0; i < values1.length; i += 1) {
+    if (!values2[i]) {
        break;
     }
 
-    if (ver1Values[i] < ver2Values[i]) {
+    if (values1[i] < values2[i]) {
       return -1;
-    } else if (ver1Values[i] > ver2Values[i]) {
+    } else if (values1[i] > values2[i]) {
       return 1;
     }
   }
 
-  if (ver1Values.length > ver2Values.length) {
+  if (values1.length > values2.length) {
     return 1;
-  } else if (ver1Values.length < ver2Values.length) {
+  } else if (values1.length < values2.length) {
     return -1;
   } else {
     return 0;
@@ -46,11 +46,19 @@ function removeTrailing0s(arr) {
     }
   }
 }
-console.log(compareVersions('0.a1', '1.*.0')); // null
-console.log(compareVersions('0.a1', '1')); // null
-console.log(compareVersions('0.1', '1')); // -1
+
+console.log(compareVersions('1.2', '1.2.0.0')); // 0
+console.log(compareVersions('1.0.0.0.0', '1')); // 0
 console.log(compareVersions('1.0', '1')); // 0
 console.log(compareVersions('1.1', '1')); // 1
+console.log(compareVersions('4.1', '2.34')); // 1
+console.log(compareVersions('1.0.0', '1.3')); // -1
 console.log(compareVersions('1.2', '1.3')); // -1
-console.log(compareVersions('1.2', '1.2.0.0')); // 0
+console.log(compareVersions('1.0', '1.0.3')); // -1
+console.log(compareVersions('0.1', '1')); // -1
 console.log(compareVersions('1.18.2', '13.37')); // -1
+// invalid inputs
+console.log(compareVersions('0.1', '1.')); // null
+console.log(compareVersions('0.a1', '1.*.0')); // null
+console.log(compareVersions('0.a1', '1')); // null
+console.log(compareVersions('.a1', '1..0')); // null
