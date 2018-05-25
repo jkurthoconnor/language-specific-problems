991dfdd61780013067c4f0aa758ecba4ef3b1cdd
jkurthoconnor <jkurthoconnor@gmail.com>
2018-05-25 16:43:30 -0600

    Add edge test case and alternate solution.

--- crs210/js/lesson8/matchingParens.js
+++ crs210/js/lesson8/matchingParens.js
@@ -1,3 +1,26 @@
+/* a more straight-forward approach:
+ *
+function isBalanced(str) {
+  var i = 0;
+  var count = 0;
+
+  for (; i < str.length; i += 1) {
+    if (str[i] === '(') {
+      count += 1;
+    } else if (str[i] === ')') {
+      count -= 1;
+    }
+
+    if (count < 0) {
+      return false;
+    }
+  }
+
+  return count === 0;
+}
+
+*/
+
 function isBalanced(str) {
   var i;
   var parens = str.replace(/[^\(\)]/g, '');
@@ -26,3 +49,4 @@ console.log(isBalanced('((What)) (is this))?'));   // false
 console.log(isBalanced('Hey!'));                   // true
 console.log(isBalanced(')Hey!('));                 // false
 console.log(isBalanced('What ((is))) up('));       // false
+console.log(isBalanced(''));                       // true
