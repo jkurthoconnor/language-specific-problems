92dd414f778c87cb30a8fb0331e4c777d9da97bf
jkurthoconnor <jkurthoconnor@gmail.com>
2018-05-25 14:30:13 -0600

    Add new edge test cases and update solution.

--- crs210/js/lesson8/acronym.js
+++ crs210/js/lesson8/acronym.js
@@ -1,23 +1,23 @@
-// using `split` regex to 'drop' hyphens to normalize data
-//
 function acronym(string) {
-  return string.split(/\W+/).map(function (word) {
-    return word[0].toUpperCase();
-  }).join('');
-}
-/* solution with separate use of `replace` to normalize
-  string prior to splitting
+  var cleanStr;
 
-function acronym(string) {
-  return string.replace('-', ' ').split(' ').map(function (word) {
+  if (!string) {
+    return  'ERROR: no acronym possible.';
+  }
+
+  cleanStr = string.replace(/-/g, ' ');
+
+  return cleanStr.split(/\s+/).map(function (word) {
     return word[0].toUpperCase();
   }).join('');
 }
-*/
+
 
 console.log(acronym('Portable Network Graphics'));                  // "PNG"
 console.log(acronym('First In, First Out'));                        // "FIFO"
 console.log(acronym('PHP: HyperText Preprocessor'));                // "PHP"
 console.log(acronym('Complementary metal-oxide semiconductor'));    // "CMOS"
 console.log(acronym('Hyper-text Markup Language'));                 // "HTML"
-
+console.log(acronym('Gnu\'s not Unix'));                 // "GNU"
+console.log(acronym(''));                 // should present an error message
+console.log(acronym('Hyper- text Markup-Language'));                 // "HTML"
