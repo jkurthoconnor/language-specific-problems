f235e449103e7234e8701b6b0f5a5e11a7835195
jkurthoconnor <jkurthoconnor@gmail.com>
2018-08-02 17:54:34 -0600

    basic features implemented

---
+++ crs230/calculator/javascripts/calculator.js
@@ -0,0 +1,28 @@
+$(function () {
+
+  $('form').on('submit', function (event) {
+    event.preventDefault();
+
+    var result;
+    var numerator = parseInt($('#numerator').val(), 10);
+    var operator = $('#operator').val();
+    var denominator = parseInt($('#denominator').val(), 10);
+    
+    switch (operator) {
+      case '+':
+        result = numerator + denominator;
+        break;
+      case '-':
+        result = numerator - denominator;
+        break;
+      case '*':
+        result = numerator * denominator;
+        break;
+      case '/':
+        result = numerator / denominator;
+        break;
+    }
+
+    $('h1').text(result.toString(10));
+  });
+});
