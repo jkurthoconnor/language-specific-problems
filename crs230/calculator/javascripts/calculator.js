8d0bdefa2374c3df800ed93e4d5adb68a1abd851
jkurthoconnor <jkurthoconnor@gmail.com>
2018-08-02 18:16:13 -0600

    assign default value to `result`

--- crs230/calculator/javascripts/calculator.js
+++ crs230/calculator/javascripts/calculator.js
@@ -3,11 +3,11 @@ $(function () {
   $('form').on('submit', function (event) {
     event.preventDefault();
 
-    var result;
+    var result = 0;
     var numerator = parseInt($('#numerator').val(), 10);
     var operator = $('#operator').val();
     var denominator = parseInt($('#denominator').val(), 10);
-    
+
     switch (operator) {
       case '+':
         result = numerator + denominator;
