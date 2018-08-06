b0d180d7024b13eb5919a5b5c6240106dcaeb47a
jkurthoconnor <jkurthoconnor@gmail.com>
2018-08-06 10:12:12 -0600

    handle modal and layer open

--- crs230/company_site/js/teamModal.js
+++ crs230/company_site/js/teamModal.js
@@ -1,6 +1,14 @@
 $(function() {
-  alert('loaded');
+  $('#team a').on('click', function(e) {
+    e.preventDefault();
 
+    var $memberLi = $(e.target).parent();
+    var $memberModal = $memberLi.find('.modal');
+    var $memberScreen = $memberLi.find('.modal_layer');
+
+    $memberModal.css('display', 'block');
+    $memberScreen.css('display', 'block');
+  });
 
 
 });
