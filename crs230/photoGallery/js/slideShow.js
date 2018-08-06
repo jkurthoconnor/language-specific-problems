716dcd2991d9751fcddf4a5eb119917f42f4827c
jkurthoconnor <jkurthoconnor@gmail.com>
2018-08-06 16:19:55 -0600

    simplify use of `$current`

--- crs230/photoGallery/js/slideShow.js
+++ crs230/photoGallery/js/slideShow.js
@@ -1,17 +1,17 @@
 $(document).ready(function(e) {
   var $mainImg = $('figure > img');
   var $thumbnails = $('li > img');
+  var $current = $thumbnails.filter('.active');
 
   $thumbnails.on('click', function(e) {
-    var current;
 
-    $thumbnails.filter('.active').removeClass('active');
+    $current.removeClass('active');
     $(e.target).addClass('active'); 
+    $current = $thumbnails.filter('.active');
 
-    current = $thumbnails.filter('.active');
     $mainImg.fadeOut(300);
     setTimeout(function() {
-      $mainImg.attr('src', current.attr('src'));
+      $mainImg.attr('src', $current.attr('src'));
     }, 300);
     $mainImg.fadeIn(900);
   });
