90c164af6506c7f63fed1dafc1464de047fba1ce
jkurthoconnor <jkurthoconnor@gmail.com>
2018-08-06 21:21:18 -0600

    refactor: Luhn method extract

--- crs230/refactoring/javascripts/to_refactor.js
+++ crs230/refactoring/javascripts/to_refactor.js
@@ -1,4 +1,29 @@
 $(function() {
+
+  function validNumber(n) {
+    var odd_total = 0;
+    var even_total = 0;
+    var cc_number = n.split("").reverse();
+
+    for (var i = 0, len = cc_number.length; i < len; i++) {
+      if (i % 2 == 1) {
+        cc_number[i] = (+cc_number[i] * 2) + "";
+        if (cc_number[i].length > 1) {
+          cc_number[i] = +cc_number[i][0] + +cc_number[i][1];
+        }
+        else {
+          cc_number[i] = +cc_number[i];
+        }
+        odd_total += cc_number[i];
+      }
+      else {
+        even_total += +cc_number[i];
+      }
+    }
+
+    return (odd_total + even_total) % 10 == 0;
+  };
+
   $("nav a").on("mouseenter", function() {
     $(this).next("ul").addClass('opened');
   });
@@ -15,43 +40,26 @@ $(function() {
   $(".toggle").on("click", function(e) {
     e.preventDefault();
 
-    if ($(this).next(".accordion").hasClass("opened")) {
-      $(this).next(".accordion").removeClass("opened");
-    }
-    else {
-      $(this).next(".accordion").addClass("opened");
-    }
+    $(this).next('.accordion').toggleClass('opened');
   });
 
   $("form").on("submit", function(e) {
     e.preventDefault();
-    var cc_number = $(this).find("[type=text]").val(),
-        odd_total = 0,
-        even_total = 0;
 
-    cc_number = cc_number.split("").reverse();
-    for (var i = 0, len = cc_number.length; i < len; i++) {
-      if (i % 2 == 1) {
-        cc_number[i] = (+cc_number[i] * 2) + "";
-        if (cc_number[i].length > 1) {
-          cc_number[i] = +cc_number[i][0] + +cc_number[i][1];
-        }
-        else {
-          cc_number[i] = +cc_number[i];
-        }
-        odd_total += cc_number[i];
-      }
-      else {
-        even_total += +cc_number[i];
-      }
-    }
-    if ((odd_total + even_total) % 10 == 0) {
-      $(this).find(".success").show();
-      $(this).find(".error").hide();
+    var $success = $(this).find('.success');
+    var $error = $(this).find('.error')
+    var cc_number = $(this).find("[type=text]").val();
+
+    $success.removeClass('opened');
+    $error.removeClass('opened');
+
+    $('form').get(0).reset();
+
+    if (validNumber(cc_number)) {
+      $success.toggleClass('opened');
     }
     else {
-      $(this).find(".error").show();
-      $(this).find(".success").hide();
+      $error.toggleClass('opened');
     }
   });
 
