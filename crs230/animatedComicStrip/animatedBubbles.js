25147057e99265f1830e563562548fcef9f55b98
jkurthoconnor <jkurthoconnor@gmail.com>
2018-08-07 16:49:56 -0600

    funcitonally complete with custom animations

--- crs230/animatedComicStrip/animatedBubbles.js
+++ crs230/animatedComicStrip/animatedBubbles.js
@@ -4,7 +4,12 @@ $(function () {
   var $blinds = $('[id^=blind]');
 
   $blinds.each(function(idx) {
-    $(this).delay(idx * defaultDelay).slideUp(speed);
+    var props = {
+      height: 0,
+      top: parseInt($(this).css('top'), 10) + parseInt($(this).css('height'), 10),
+    }
+
+    $(this).delay(idx * defaultDelay).animate(props, speed);
   });
 });
 
