d00a376c5c5b5f3cff61729e9e67a340e6b324f1
jkurthoconnor <jkurthoconnor@gmail.com>
2018-08-07 17:46:20 -0600

    implement Run Again link

--- crs230/animatedComicStrip/animatedBubbles.js
+++ crs230/animatedComicStrip/animatedBubbles.js
@@ -1,15 +1,31 @@
 $(function () {
-  var speed = 250;
-  var defaultDelay = 1500;
+
   var $blinds = $('[id^=blind]');
 
-  $blinds.each(function(idx) {
-    var props = {
-      height: 0,
-      top: parseInt($(this).css('top'), 10) + parseInt($(this).css('height'), 10),
-    }
+  function showText() {
+    var speed = 250;
+    var defaultDelay = 1500;
 
-    $(this).delay(idx * defaultDelay).animate(props, speed);
+    $blinds.each(function(idx) {
+      var props = {
+        height: 0,
+        top: parseInt($(this).css('top'), 10) + parseInt($(this).css('height'), 10),
+      }
+
+      $(this).delay(idx * defaultDelay).animate(props, speed);
+    });
+  }
+
+  showText();
+
+  $('p a').on('click', function(e) {
+    e.preventDefault();
+
+    $blinds.each(function() {
+      $(this).attr('style', '');
+    });
+
+    showText();
   });
 });
 
