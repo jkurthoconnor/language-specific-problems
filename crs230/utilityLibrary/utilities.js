be40059779375d1779bfb7b623b0319062b3f5e5
jkurthoconnor <jkurthoconnor@gmail.com>
2018-08-09 15:16:38 -0600

    all methods green

--- crs230/utilityLibrary/utilities.js
+++ crs230/utilityLibrary/utilities.js
@@ -42,6 +42,7 @@
 
       findWhere: function(obj) {
         var idx;
+        var key;
         var targetKey = 0;
         var keys = Object.keys(obj);
 
@@ -58,6 +59,7 @@
           }                               // be checked before the last
        }                                 // b/f `idx` loop ends to
                                          // catch match on last obj
+
         return undefined;
       },
 
@@ -72,11 +74,145 @@
         });
 
         return holder;
-      }
+      },
+
+      pluck: function(key) {
+        var holder = [];
+
+        arg.forEach(function(obj) {
+          if (obj.hasOwnProperty(key)) {
+            holder.push(obj[key]);
+          }
+        });
+
+        return holder;
+      },
+
+      keys: function() {
+        return Object.getOwnPropertyNames(arg);
+      },
+
+      values: function() {
+        var holder = [];
+        var keys = Object.getOwnPropertyNames(arg);
+
+        keys.forEach(function(key) {
+          holder.push(arg[key]);
+        });
+
+        return holder;
+      },
+
+      pick: function(properties) {
+        var properties = Array.prototype.slice.call(arguments);
+        var newObj = {};
+
+        properties.forEach(function(property) {
+          if (arg.hasOwnProperty(property)) {
+            newObj[property] = arg[property];
+          }
+        });
+
+        return newObj;
+      },
+
+      omit: function() {
+        var toOmit = Array.prototype.slice.call(arguments);
+        var newObj = {};
+        var originalProperties = Object.getOwnPropertyNames(arg);
+
+        originalProperties.forEach(function(property) {
+          if (!toOmit.includes(property)) {
+            newObj[property] = arg[property];
+          }
+        });
+
+        return newObj;
+      },
+
+      has: function(property) {
+        return property in arg;
+      },
+
+      isElement: function() {
+        return _.isElement(arg);
+      },
+
+      isArray: function() {
+        return _.isArray(arg);
+      },
+
+      isObject: function() {
+        return _.isObject(arg);
+      },
+
+      isFunction: function() {
+//        return _.isFunction(arg);
+        _.isFunction.call(this, arg);
+      },
+
+      isString: function() {
+        return _.isString(arg);
+      },
+
+      isNumber: function() {
+        return _.isNumber(arg);
+      },
+
+      isBoolean: function() {
+        return _.isBoolean(arg);
+      },
     };
 
     return u;
   };
+
+  _.isArray = function(value) {
+    return Array.isArray(value);
+  };
+
+  _.isElement = function(value) {
+      return value && value.nodeType === 1;
+  };
+
+  _.isObject = function(value) {
+    return ['object', 'function'].includes(typeof value);
+  };
+
+  _.isFunction = function(value) {
+    return (typeof value === 'function');
+  };
+
+  _.isString = function(value) {
+    return (typeof value === 'string' || value instanceof String);
+  };
+
+  _.isNumber = function(value) {
+    return (typeof value === 'number' || value instanceof Number);
+  };
+
+  _.isBoolean = function(value) {
+    return (typeof value === 'boolean' || value instanceof Boolean);
+  };
+
+  _.extend = function() {
+    function combineProperties(recipient, source) {
+      var properties = Object.getOwnPropertyNames(source);
+
+      properties.forEach(function(prop) {
+        recipient[prop] = source[prop];
+      });
+    }
+
+    var objects = Array.prototype.slice.call(arguments);
+    var idx;
+
+    for (idx = objects.length - 2; idx >= 0; idx -= 1) {
+      combineProperties(objects[idx], objects[idx + 1]);
+    }
+
+    return objects[0];
+  },
 
   _.range = function(n1, n2) {
     var result = [];
