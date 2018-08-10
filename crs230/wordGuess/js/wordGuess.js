74e13d477fc1b9fa2b6c293b645fd0964e958d4c
jkurthoconnor <jkurthoconnor@gmail.com>
2018-08-09 18:18:28 -0600

    implement core functions (spike)

--- crs230/wordGuess/js/wordGuess.js
+++ crs230/wordGuess/js/wordGuess.js
@@ -10,9 +10,45 @@ $(document).ready(function() {
 
       return word;
     };
-  })();  
+  })();
+
+  var Game = {
+    word:  '',
+    wrongGuesses: 0,
+    guesses: [],
+    maxWrongGuesses: 6,
+    message: '',
+    makeSpaces: function(n) {
+      var i;
+      for (i = 1; i <= n; i += 1) {
+        $('#spaces > h2').append($('<span>'));
+      }
+    },
+
+    init: function() {
+      this.word = randomWord();
+      this.message = this.word ? 'Time to Play!' : 'Sorry, out of words!';
+      $('#message').text(this.message);
+      this.makeSpaces(this.word.length);
+      return this;
+    },
+  };
 
 
 
+  var game1 = Object.create(Game).init();
+  console.log(game1.word);
+  console.log(game1.message);
+  /*
+  var game2 = Object.create(Game).init();
+  console.log(game2.word);
+  var game3 = Object.create(Game).init();
+  console.log(game3.word);
+  var game4 = Object.create(Game).init();
+  console.log(game4.word);
+  var game5 = Object.create(Game).init();
+  console.log(game5.word);
+  console.log(game5.message);
+*/
 
 });
