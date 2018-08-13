f2d0cd293e07da4d3ba869b1fac39f3a95924c5c
jkurthoconnor <jkurthoconnor@gmail.com>
2018-08-13 17:43:56 -0600

    add delete functionality

--- crs230/manageCollections/js/inventory.js
+++ crs230/manageCollections/js/inventory.js
@@ -1,9 +1,14 @@
 var inventory;
 
 (function() {
+
+  var id = 0;
+
   inventory = {
 
     collection: [],
+
+    lastId: 0,
 
     setDate: function() {
       var date = new Date();
@@ -14,9 +19,26 @@ var inventory;
     template: '',
 
     setTemplate: function() {
-      var $template = $('#inventory_item');
-      this.template = $template.text().trim();
-      $template.remove();
+      var $template = $('#inventory_item').remove();
+      this.template = $template.html();
+    },
+
+    item: {
+      id: 0,
+      name: '',
+      stockNumber: '',
+      quantity: 1,
+    },
+
+    addToCollection: function() {
+      var newItem = Object.create(this.item);
+      newItem.id = id++;
+      this.lastId = id;
+      this.collection.push(newItem);
+    },
+
+    alterRowID: function(rowTemplate) {
+      return rowTemplate.replace(/ID/g, this.lastId);
     },
 
     init: function() {
@@ -28,3 +50,23 @@ var inventory;
 
 
 $(inventory.init.bind(inventory));
+
+$(function(e) {
+  var $inventoryTable = $('#inventory');
+
+  $('#add_item').on('click', function(e) {
+    $inventoryTable.append(inventory.alterRowID(inventory.template));
+    inventory.addToCollection();
+  });
+
+  $inventoryTable.on('click', 'td > a.delete', function(e) {
+    e.preventDefault();
+
+    var $row = $(this).closest('tr').remove();
+    var collectionIdx = $row.find('input[type="hidden"]').val();
+
+    inventory.collection.splice(collectionIdx, 1);
+  });
+
+});
+
