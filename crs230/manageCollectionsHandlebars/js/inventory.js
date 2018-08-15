2c5aeecd3ff7bdcd7fcc1987da2cfa75ab77a351
jkurthoconnor <jkurthoconnor@gmail.com>
2018-08-14 18:20:03 -0600

    use Handlebars in place of strings and `replace`

---
+++ crs230/manageCollectionsHandlebars/js/inventory.js
@@ -0,0 +1,88 @@
+var inventory;
+
+(function() {
+  var index = 0;
+
+  inventory = {
+
+    collection: [],
+
+    setDate: function() {
+      var date = new Date();
+      $('#order_date').text(date.toDateString());
+
+    },
+
+    templateRowScript: null,
+
+    setTemplate: function() {
+      var $template = $('#inventory_item').remove();
+      this.templateRowScript = Handlebars.compile($template.html());
+    },
+
+    item: {
+      id: 0,
+      name: '',
+      stockNumber: '',
+      quantity: 1,
+    },
+
+    addToCollection: function() {
+      var newItem = Object.create(this.item);
+      newItem.id = index++;
+      this.collection.push(newItem);
+    },
+
+    updateCollection: function(data) {
+      var item = this.collection[data.idx];
+      var category = data.category;
+      var value = data.value;
+
+      category = category.match(/^s/) ? 'stockNumber' : category;
+
+      if (category === 'quantity') {
+        value = Number(value);
+      }
+
+      if (value !== undefined) {
+        item[category] = value;
+      }
+    },
+
+    setListeners: function() {
+      var $inventoryTable = $('#inventory');
+
+      $('#add_item').on('click', function(e) {
+        $inventoryTable.append(inventory.templateRowScript({id: String(index)}));
+        inventory.addToCollection();
+      });
+
+      $inventoryTable.on('click', 'td > a.delete', function(e) {
+        e.preventDefault();
+
+        var $row = $(this).closest('tr').remove();
+        var itemIdx = Number($row.find('input[type="hidden"]').val());
+
+        inventory.collection.splice(itemIdx, 1);
+      });
+
+      $inventoryTable.on('blur', 'input', function(e) {
+        var input = $(this).attr('name').split(/_/g);
+        var itemIdx = Number(input.slice(-1)[0]);
+        var itemCategory = input.slice(1, 2)[0];
+        var itemValue = $(this).val().trim();
+
+        inventory.updateCollection({idx: itemIdx, category: itemCategory, value: itemValue});
+      });
+    },
+
+    init: function() {
+      this.setDate();
+      this.setTemplate();
+      this.setListeners();
+    },
+  };
+})();
+
+$(inventory.init.bind(inventory));
+
