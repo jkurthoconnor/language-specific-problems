5c33354601348e5f3e0879ff8af584dffec7b34d
jkurthoconnor <jkurthoconnor@gmail.com>
2018-08-15 12:36:35 -0600

    implement photo & information templates

--- crs230/photo_gallery2/public/javascripts/gallery.js
+++ crs230/photo_gallery2/public/javascripts/gallery.js
@@ -6,7 +6,15 @@ $(document).ready(function() {
     url: '/photos',
   })
   .done(function(json) {
-    console.log(json);
+    var photosTemplate = $('#photos').html();
+    var photoInfoTemplate = $('#photo_information').html();
+    var photosTemplateScript = Handlebars.compile(photosTemplate);
+    var photoInfoTemplateScript = Handlebars.compile(photoInfoTemplate);
+
+    $('#slides').append(photosTemplateScript( {photos: json} ));
+    $('section > header').append(photoInfoTemplateScript(json[0]));
+
+    console.log(json[0]);
   });
 
 });
